9e360603e9eb7e2fd5c771c6f325fc3b430749f8
michael magner <magner@knotch.com>
2019-04-30 15:35:30 -0400

    working repo

---
+++ lib/rumbl/repo.ex
@@ -0,0 +1,54 @@
+defmodule Rumbl.Repo do
+  # use Ecto.Repo,
+  #   otp_app: :rumbl,
+  #   adapter: Ecto.Adapters.Postgres
+
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]},
+      type: :worker,
+      restart: :permanent,
+      shutdown: 500
+    }
+  end
+
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  ## Callbacks
+
+  @impl true
+  def init(stack) do
+    {:ok, stack}
+  end
+
+  @impl true
+  def handle_call(:pop, _from, [head | tail]) do
+    {:reply, head, tail}
+  end
+
+  @impl true
+  def handle_cast({:push, head}, tail) do
+    {:noreply, [head | tail]}
+  end
+
+
+  def all(Rumbl.User) do
+    [%Rumbl.User{id: "1", name: "José", username: "josevalim", password: "elixir"},
+      %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
+      %Rumbl.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}]
+  end
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+  end
+end
